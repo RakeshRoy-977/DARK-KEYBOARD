935412d0b8b573728836fc86cf9f85dcedb0eeaa
RakeshRoy-977 <15rakeshroy@gmail.com>
2024-03-07 15:33:10 +0530

    Update Navbar

--- Client/src/Components/Navbar.jsx
+++ Client/src/Components/Navbar.jsx
@@ -1,112 +1,101 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { logout, selectIsLoggedIn } from "../Store/isLoggedInSlice";
 
 const Navbar = () => {
-  const isLoggedIn = useSelector(selectIsLoggedIn);
-  const dispatch = useDispatch();
+  const [isOpen, setIsOpen] = useState(false);
 
-  const handelLogout = () => {
-    // Remove token from cookies
-    document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-    dispatch(logout());
-  };
-
-  const renderLoginLogoutButton = () => {
-    if (isLoggedIn) {
-      return (
-        <div className="hidden sm:flex">
-          <Link
-            to="/"
-            onClick={handelLogout}
-            className="rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600"
-          >
-            Logout
+  return (
+    <nav className=" shadow-lg">
+      <div className="mx-auto px-4 py-2 max-w-screen-xl flex justify-between items-center">
+        <div className="flex items-center">
+          <Link to="/" className=" font-bold">
+            DARK KEYBOARD
           </Link>
         </div>
-      );
-    } else {
-      return (
-        <>
+        <div className="hidden md:flex md:space-x-4">
+          <Link to="/SumitJokes" className="">
+            Submit Joke
+          </Link>
+          <Link to="/ContactUsPage" className="">
+            Contact Us
+          </Link>
+        </div>
+        <div className="hidden md:flex md:space-x-4">
           <Link
             to="/login"
             className="rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white shadow"
           >
             Login
           </Link>
-          <div className="hidden sm:flex">
-            <Link
-              to="/signup"
-              className="rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600"
+          <Link
+            to="/signup"
+            className="rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600"
+          >
+            Signup
+          </Link>
+        </div>
+        <div className="md:hidden">
+          <button
+            onClick={() => setIsOpen(!isOpen)}
+            className=" focus:outline-none"
+          >
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
             >
-              Register
-            </Link>
-          </div>
-        </>
-      );
-    }
-  };
-
-  return (
-    <header className="bg-white">
-      <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
-        <div className="flex h-16 items-center justify-between">
-          <div className="md:flex md:items-center md:gap-12">
-            <Link className="" to={"/"}>
-              DARK KEYBOARD
-            </Link>
-          </div>
-
-          <div className="hidden md:block">
-            <nav aria-label="Global">
-              <ul className="flex items-center gap-6 text-sm">
-                <li>
-                  <Link to="/Ranking" className="font-semibold">
-                    Top Ranks
-                  </Link>
-                </li>
-
-                <li>
-                  <Link to="/ContactUsPage" className="font-semibold">
-                    Contact Us
-                  </Link>
-                </li>
-
-                <li>
-                  <Link to="/SumitJokes" className="font-semibold">
-                    Sumit Jokes
-                  </Link>
-                </li>
-              </ul>
-            </nav>
-          </div>
-
-          <div className="flex items-center gap-4">
-            <div className="sm:flex sm:gap-4">{renderLoginLogoutButton()}</div>
-
-            <div className="block md:hidden">
-              <button className="rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
+              {isOpen ? (
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                   strokeWidth="2"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  />
-                </svg>
-              </button>
-            </div>
-          </div>
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              ) : (
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16m-7 6h7"
+                />
+              )}
+            </svg>
+          </button>
         </div>
       </div>
-    </header>
+      {isOpen && (
+        <div className="md:hidden">
+          <div className="px-2 pt-2 pb-4 space-y-1">
+            <Link
+              to="/SumitJokes"
+              className="block px-3 py-2  rounded-md hover:bg-gray-700"
+            >
+              Submit Joke
+            </Link>
+            <Link
+              to="/ContactUsPage"
+              className="block px-3 py-2  rounded-md hover:bg-gray-700"
+            >
+              Contact Us
+            </Link>
+            <Link
+              to="/login"
+              className="block px-3 py-2  rounded-md hover:bg-gray-700"
+            >
+              Login
+            </Link>
+            <Link
+              to="/signup"
+              className="block px-3 py-2  rounded-md hover:bg-gray-700"
+            >
+              SignUp
+            </Link>
+          </div>
+        </div>
+      )}
+    </nav>
   );
 };
 
